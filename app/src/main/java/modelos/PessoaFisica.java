d563135d53a1e7aad47785d9f7a564e8c834e251
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2024-02-23 01:39:43 +0000

    Classes Pessoa e PessoaFisica

---
+++ app/src/main/java/modelos/PessoaFisica.java
@@ -0,0 +1,13 @@
+package modelos;
+
+public class PessoaFisica {
+    private int idade;
+
+    public void setNome(int idade) {
+        this.idade = idade;
+    }
+
+    public int setIdade() {
+        return this.idade;
+    }
+}
--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -1,7 +1,15 @@
 package primeiro_exemplo;
 
+import modelos.Pessoa;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Funciona!!!");
+
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Marco");
+        
+
+        System.out.println(p1.getNome());
     }
 }
